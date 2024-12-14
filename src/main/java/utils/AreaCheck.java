a348c928d18ac77e7898783f01ee9418558f186c
danis <ahrnadeev@gmail.com>
2024-12-14 07:19:55 +0300

    Fix area check

--- src/main/java/utils/AreaCheck.java
+++ src/main/java/utils/AreaCheck.java
@@ -13,32 +13,28 @@ public class AreaCheck {
 
     public boolean isHit(double x, double y, double r) {
         if (r > 0) {
-            // Top-right (Quarter circle)
-            if (x >= 0 && y >= 0) {
+            if (x > 0 && y >= 0) {
                 return (x * x + y * y) <= (r / 2) * (r / 2);
             }
-            // Top-left (Rectangle)
             if (x <= 0 && y >= 0) {
                 return (y <= r) && (x >= (-r) / 2);
             }
-            // Bottom-left (Triangle)
             if (x <= 0 && y <= 0) {
                 return (y >= -x - r / 2);
             }
         } else if (r < 0) {
-            // Reflecting the shapes when r < 0
+            x = -x;
+            y = -y;
+            r = -r;
 
-            // Bottom-left (Quarter circle, reflected to top-right)
-            if (x <= 0 && y <= 0) {
+            if (x < 0 && y <= 0) {
                 return (x * x + y * y) <= (r / 2) * (r / 2);
             }
-            // Bottom-right (Rectangle, reflected to top-left)
             if (x >= 0 && y <= 0) {
-                return (y >= r) && (x <= (-r) / 2);
+                return (y >= -r) && (x <= r / 2);
             }
-            // Top-right (Triangle, reflected to bottom-left)
             if (x >= 0 && y >= 0) {
-                return (y <= -x - r / 2);
+                return (y <= -x + r / 2);
             }
         }
         return false;
